2f494c5f7ff2060934fd7c0797e33a18f364045c
Mehmet Çürükoğlu <mehmetcrkglu@gmail.com>
2020-09-26 15:34:05 +0300

    first commit

---
+++ ShoppingCard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShoppingCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
